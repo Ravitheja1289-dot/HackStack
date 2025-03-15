bef01830c24062c1fa602556150e351dbdd39c72
Sai Anjan Kumar Tanneeru <tsanjankumar@gmail.com>
2025-03-15 12:08:15 +0530

    frontend

--- my-app/src/components/Charts.jsx
+++ my-app/src/components/Charts.jsx
@@ -1,9 +1,23 @@
+import { useState ,useEffect} from "react";
 import React from "react";
-const Charts =() =>{
-
-    return(
-        <>
-    </>
-);
-};
-export default Charts
+function Charts() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
+        .then((response) => response.json())
+        .then((data) => setData(data));
+    }, []);
+    return (
+        <div>
+        <h1>Charts</h1>
+        <ul>
+            {data.map((item) => (
+            <li key={item.id}>
+                {item.name} {item.current_price}
+            </li>
+            ))}
+        </ul>
+        </div>
+    );
+    }
+    export default Charts;
