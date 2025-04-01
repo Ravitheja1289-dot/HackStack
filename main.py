f69a584a62f5fc95d9d9f7a315dd6d5fc36fa497
Ravitheja Reddy <ravithejareddy1289@gmail.com>
2025-04-01 21:10:00 +0530

    deleted

--- main.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDyFUmT8OtZdJDHzc0R1Ro-pBdJrSjIkDc")
-
-models = genai.list_models()
-for model in models:
-    print(model.name)
